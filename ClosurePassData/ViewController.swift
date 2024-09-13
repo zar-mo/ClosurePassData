bd5c6935acd5447a2aca82967476ea96bd732e17
Abouzar <abouzarmoradian1@gmail.com>
2024-09-12 21:29:03 -0400

    added some thoughts

--- ClosurePassData/ViewController.swift
+++ ClosurePassData/ViewController.swift
@@ -5,6 +5,9 @@
 //  Created by Abouzar Moradian on 9/11/24.
 //
 
+
+
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -36,3 +39,20 @@ class ViewController: UIViewController {
     
 }
 
+// MARK: using closure and using delegate for passing data are very similar and in fact follow the same logic. the idea is that you define the signiture of a function in the child view and force the parent view to implement it. then in the child view you call this optional function (method) and pass data in it. in parent view we can access the passed data and do whatever we want with it.
+
+/*
+ child view:   delegate: ((String) -> ())?
+               delegate?("abc", "xyz")
+ 
+ parent view:  childView.delegate = {(String, String) -> () in [body of closure]}
+ */
+
+/*
+child view:   delegate: DelegateProtocol?
+              delegate?.method("abc", "xyz")
+ 
+ parent view:   childView.delegate = self
+                delegateFunc(String, String) -> (){ [body of method]}
+ 
+ */
